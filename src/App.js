3e5957cfccafa6229a26b44b55dbcc16a1c1e6f1
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-18 17:38:48 +0900

    contact card added with props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -10,6 +11,12 @@ function App() {
       <Navbar />
       <Hero />
       <Card />
+      <Contact
+        img="./images/mr-whiskerson.png"
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
     </div>
   );
 }
