117260a4398f4decaa82d6f915b122cd85467586
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-18 18:54:36 +0900

    joke component added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
 import Contact from './components/Contact';
+import Joke from './components/Joke';
 
 function App() {
   return (
@@ -16,6 +17,26 @@ function App() {
         name="Mr. Whiskerson"
         phone="(212) 555-1234"
         email="mr.whiskaz@catnap.meow"
+      />
+      <Joke
+        setup="I got my daughter a fridge for her birthday."
+        punchline="I can't wait to see her face light up when she opens it."
+      />
+      <Joke
+        setup="How did the hacker escape the police?"
+        punchline="He just ransomware!"
+      />
+      <Joke
+        setup="Why don't pirates travel on mountain roads?"
+        punchline="Scurvy"
+      />
+      <Joke
+        setup="Why do bees stay in the hive in the winter?"
+        punchline="Swarm"
+      />
+      <Joke
+        setup="What's the best thing about Switzerland?"
+        punchline="I don't know, but the flag is a big plus!"
       />
     </div>
   );
