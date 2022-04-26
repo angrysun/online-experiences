8820ba04b779f901b8316c6ed0cfdf0ddde5ad18
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-26 14:43:59 +0900

    cards updated to get data from other file

--- src/App.js
+++ src/App.js
@@ -3,34 +3,33 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-import Contact from './components/Contact';
-import Joke from './components/Joke';
-import jokesData from './components/jokesData';
+// import Contact from './components/Contact';
+// import Joke from './components/Joke';
+// import jokesData from './jokesData';
+import data from './data';
 
 function App() {
-  const jokeElements = jokesData.map(joke => {
-    return <Joke setup={joke.setup} punchline={joke.punchline} />
+  const cards = data.map(card => {
+    return <Card
+      img={card.coverImg}
+      rating={card.stats.rating}
+      reviewCount={card.stats.reviewCount}
+      location={card.location}
+      title={card.title}
+      price={card.price}
+    />
   })
+  // const jokeElements = jokesData.map(joke => {
+  //   return <Joke setup={joke.setup} punchline={joke.punchline} />
+  // })
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
-      <Contact
-        img="./images/mr-whiskerson.png"
-        name="Mr. Whiskerson"
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
-      />
-      <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
-      {jokeElements}
+      <section className='cards-list'>
+        {cards}
+      </section>
+      {/* {jokeElements} */}
     </div>
   );
 }
