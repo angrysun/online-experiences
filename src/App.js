52b0543be3532d50c79a91e784690dbc560cbc50
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-26 16:37:45 +0900

    shortened code for Card component

--- src/App.js
+++ src/App.js
@@ -9,16 +9,10 @@ import Card from './components/Card';
 import data from './data';
 
 function App() {
-  const cards = data.map(card => {
+  const cards = data.map(item => {
     return <Card
-      key={card.id}
-      img={card.coverImg}
-      rating={card.stats.rating}
-      reviewCount={card.stats.reviewCount}
-      location={card.location}
-      title={card.title}
-      price={card.price}
-      openSpots={card.openSpots}
+      key={item.id}
+      item={item}
     />
   })
   // const jokeElements = jokesData.map(joke => {
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 export default function Card(props) {
   let badgeText
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT"
-  } else if (props.location === "Online") {
+  } else if (props.item.location === "Online") {
     badgeText = "ONLINE"
   }
 
@@ -10,15 +10,15 @@ export default function Card(props) {
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
       {/* {typeof badgeText != 'undefined' && <div className="card--badge">{badgeText}</div>} */}
-      {/* <img src={`../images/${props.img}`} className="card--image" /> */}
+      {/* <img src={`../images/${props.item.coverImg}`} className="card--image" /> */}
       <div className="card--stats">
         {/* <img src="../images/star.png" className="card--star" /> */}
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray">{props.country}</span>
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.country}</span>
       </div>
-      <p className="card--title">{props.title}</p>
-      <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+      <p className="card--title">{props.item.title}</p>
+      <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
     </div>
   )
 }
