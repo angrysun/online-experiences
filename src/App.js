53d79d67c61965902c349b3193fde19605b5cb11
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-26 13:31:10 +0900

    added jokesData array with jokes and updated App

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Hero from './components/Hero';
 import Card from './components/Card';
 import Contact from './components/Contact';
 import Joke from './components/Joke';
+import jokesData from './components/jokesData';
 
 function App() {
+  const jokeElements = jokesData.map(joke => {
+    return <Joke setup={joke.setup} punchline={joke.punchline} />
+  })
   return (
     <div className="App">
       <Navbar />
@@ -26,52 +30,7 @@ function App() {
         title="Life lessons with Katie Zaferes"
         price={136}
       />
-      <Joke
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-      />
-      <Joke
-        punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
-        isPun={true}
-        upvotes={10}
-        downvotes={2}
-        comments={[{ author: "", body: "", title: "" }]}
-      />
-      <Joke
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-        isPun={false}
-        upvotes={14}
-        downvotes={6}
-      />
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-        isPun={true}
-        upvotes={11}
-        downvotes={3}
-      />
-      <Joke
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-        isPun={true}
-        upvotes={11}
-        downvotes={3}
-        />
-      <Joke
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm."
-        isPun={true}
-        upvotes={11}
-        downvotes={3}
-      />
-      <Joke
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-        isPun={false}
-        upvotes={11}
-        downvotes={3}
-      />
+      {jokeElements}
     </div>
   );
 }
