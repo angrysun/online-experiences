098cdf080da1dc354709e9f9b2ecb827594a666f
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-11 13:18:26 +0900

    state added in to toggle showing a punchline

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,8 +1,21 @@
+import { useState } from "react"
+
 export default function Joke(props) {
+  const [isShown, setIsShown] = useState(false)
+  const toggleIsShown = () => setIsShown(!isShown)
+
+  // function toggleShown() {
+  //   console.log(isShown)
+  //   setIsShown(prevShown => !prevShown)
+  // }
+
   return (
     <div>
       {props.setup && <h3>Setup: {props.setup}</h3>}
       <p>Punchline: {props.punchline}</p>
+      <button onClick={toggleIsShown}>
+        Click me
+      </button>
       <hr />
     </div>
   )
